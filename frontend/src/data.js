3da8fada9ff59449bcac555b25e6ac94264a0f2d
Shameer Eranhikath <shmr_ekth@yahoo.com>
2021-01-13 22:09:13 +0400

    Initial Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,55 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike',
+            category:'Shirts',
+            image:'/images/1.jpg',
+            price:'240',
+            brand:'Adidas',
+            rating:'4.5',
+            numReviews:'10',
+            description:'Good Quality'
+        }
+        ,
+        {
+            _id:'2',
+            name:'MAX',
+            category:'Shirts',
+            image:'/images/2.jpg',
+            price:'140',
+            brand:'Adidas',
+            rating:'4.5',
+            numReviews:'10',
+            description:'Good Quality'
+        }
+        ,
+        {
+            _id:'3',
+            name:'Reebok',
+            category:'Shirts',
+            image:'/images/3.jpg',
+            price:'40',
+            brand:'Adidas',
+            rating:'4.5',
+            numReviews:'10',
+            description:'Good Quality'
+        }
+        ,
+        {
+            _id:'4',
+            name:'Davidoff',
+            category:'Shirts',
+            image:'/images/4.jpg',
+            price:'1400',
+            brand:'Adidas',
+            rating:'4.5',
+            numReviews:'10',
+            description:'Good Quality'
+        }
+        
+
+    ]
+}
+
+export default data;
